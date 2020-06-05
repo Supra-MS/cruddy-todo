0bc614429c5266cd65a7b69062f49cc915619052
Supra-MS <suprabharathi.m.s@gmail.com>
2020-06-04 18:41:10 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -62,13 +62,6 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  /* var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  } */
   var newFilePath = `${this.dataDir}/${id}.txt`;
   fs.access(newFilePath, fs.constants.F_OK, (err) => {
     if (err) {
@@ -86,14 +79,20 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var newFilePath = `${this.dataDir}/${id}.txt`;
+  fs.access(newFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.unlink(newFilePath, (err) => {
+        if (err) {
+          console.log('Error deleting the todo: ', err);
+        } else {
+          callback(null);
+        }
+      });
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
